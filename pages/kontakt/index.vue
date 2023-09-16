3361c7d68d4e9dff5c8ff37ee94fdb377e893082
LukasKalenda <kalenda.lu.2020@skola.ssps.cz>
2023-09-16 19:11:30 +0200

    zpetna

--- pages/kontakt/index.vue
+++ pages/kontakt/index.vue
@@ -97,14 +97,13 @@
               Kontaktní formulář
             </h1>
 
-            <!-- <form
+            <form
               class="mt-4"
               name="contact"
               action="/success"
               method="POST"
               data-netlify="true"
               data-netlify-recaptcha="true"
-              onSubmit="submit"
             >
               <div class="flex-1">
                 <label
@@ -159,30 +158,6 @@
               >
                 Odeslat!
               </button>
-            </form> -->
-            <form name="contact" method="POST" data-netlify="true">
-              <p>
-                <label>Your Name: <input type="text" name="name" /></label>
-              </p>
-              <p>
-                <label>Your Email: <input type="email" name="email" /></label>
-              </p>
-              <p>
-                <label
-                  >Your Role:
-                  <select name="role[]" multiple>
-                    <option value="leader">Leader</option>
-                    <option value="follower">Follower</option>
-                  </select></label
-                >
-              </p>
-              <p>
-                <label>Message: <textarea name="message"></textarea></label>
-              </p>
-              <input type="hidden" name="form-name" value="contact" />
-              <p>
-                <button type="submit">Send</button>
-              </p>
             </form>
           </div>
         </div>

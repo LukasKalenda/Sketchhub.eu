e0a9a136ed82ad4fe46c034c0af76d4f2293ae2f
LukasKalenda <kalenda.lu.2020@skola.ssps.cz>
2023-09-16 09:45:15 +0200

    Přidání alpinejs a nuxt.config meta tagu

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // devtools: { enabled: true }
+  build: {
+    transpile: ['alpinejs'], // Přidat Alpine.js do transpilace
+  },
+  app: {
+    head: {
+      title: 'Sketchub.eu',
+      meta: [
+        { name: 'description', content: 'Portál studentských projektů,3D modely, 3D tisk, plastické modely, diorámata' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'theme-color', content: '#000000' },
+  
+      ]
+    }
+  },
   modules: [
     '@nuxtjs/tailwindcss'
   ],
